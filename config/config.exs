6fa68acb98d42acaa1a1677a4414e07749f6fcfe
Dennis Njuguna <dennix.njuguna85@gmail.com>
2023-03-20 16:31:00 +0300

    use config file

---
+++ config/config.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :github_issues, github_url: "https://api.github.com"
--- lib/github_issues/cli.ex
+++ lib/github_issues/cli.ex
@@ -50,5 +50,13 @@ defmodule GithubIssues.Cli do
 
   def process({user, project, _count}) do
     GithubIssues.KnownIssues.fetch(user, project)
+    |> decode_response()
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from Github: #{error["message"]}"
+    System.halt(2)
   end
 end
